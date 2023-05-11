e2ef834745ce64d2227214800bfa46b3feae73cc
tymich <61485777+tymich@users.noreply.github.com>
2023-05-11 18:42:37 -0400

    Create init.js

---
+++ init.js
@@ -0,0 +1,27 @@
+db=db.getSiblingDB("Vinyls");
+
+db.createCollection("Album");
+db.createCollection("Record Label");
+
+db.createCollection("Artist, {
+   validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["ArtistID", "ArtistName", "ArtistYear"],
+  properties: {
+      ArtistID: {
+         bsonType: "int",
+          description: "must be an int and is required"
+      },
+      ArtistName: {
+         bsonType: "string",
+          description: "must be a string and is required"
+      },
+      ArtistYear: {
+         bsonType: "int",
+          description: "must be an int and is required"
+      },
+  }
+}
+}
+})
